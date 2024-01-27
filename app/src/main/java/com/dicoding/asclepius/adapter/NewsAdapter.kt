fd1d0f89bf12fee5a14a8b7971246604ccbe5130
ekorahy <ekorahy@gmail.com>
2024-01-28 01:43:04 +0700

    added detailed news feature

--- app/src/main/java/com/dicoding/asclepius/adapter/NewsAdapter.kt
+++ app/src/main/java/com/dicoding/asclepius/adapter/NewsAdapter.kt
@@ -1,5 +1,6 @@
 package com.dicoding.asclepius.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -9,6 +10,13 @@ import com.bumptech.glide.Glide
 import com.dicoding.asclepius.R
 import com.dicoding.asclepius.data.response.ArticlesItem
 import com.dicoding.asclepius.databinding.ItemNewsBinding
+import com.dicoding.asclepius.view.detailNews.DetailNewsActivity
+import com.dicoding.asclepius.view.detailNews.DetailNewsActivity.Companion.AUTHOR
+import com.dicoding.asclepius.view.detailNews.DetailNewsActivity.Companion.DESCRIPTION
+import com.dicoding.asclepius.view.detailNews.DetailNewsActivity.Companion.IMAGE
+import com.dicoding.asclepius.view.detailNews.DetailNewsActivity.Companion.PUBLISHED_AT
+import com.dicoding.asclepius.view.detailNews.DetailNewsActivity.Companion.SOURCE_NAME
+import com.dicoding.asclepius.view.detailNews.DetailNewsActivity.Companion.TITLE
 
 class NewsAdapter: ListAdapter<ArticlesItem, NewsAdapter.MyViewHolder>(DIFF_CALLBACK) {
 
@@ -21,14 +29,25 @@ class NewsAdapter: ListAdapter<ArticlesItem, NewsAdapter.MyViewHolder>(DIFF_CALL
         val news = getItem(position)
         holder.bind(news)
         holder.itemView.setOnClickListener {
-
+            val intent = Intent(holder.itemView.context, DetailNewsActivity::class.java)
+            intent.putExtra(TITLE, news.title)
+            intent.putExtra(AUTHOR, news.author)
+            if (news.urlToImage != null) {
+                intent.putExtra(IMAGE, news.urlToImage.toString())
+            } else {
+                intent.putExtra(IMAGE, "null")
+            }
+            intent.putExtra(SOURCE_NAME, news.source.name)
+            intent.putExtra(PUBLISHED_AT, news.publishedAt)
+            intent.putExtra(DESCRIPTION, news.description)
+            holder.itemView.context.startActivity(intent)
         }
     }
-    class MyViewHolder(val binding: ItemNewsBinding): RecyclerView.ViewHolder(binding.root) {
+    class MyViewHolder(private val binding: ItemNewsBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(news: ArticlesItem) {
             binding.tvTitle.text = news.title
             binding.tvAuthor.text = news.author
-            if (news.urlToImage !== null) {
+            if (news.urlToImage != null) {
                 Glide.with(itemView.context)
                     .load(news.urlToImage)
                     .into(binding.ivNews)
---
+++ app/src/main/java/com/dicoding/asclepius/view/detailNews/DetailNewsActivity.kt
@@ -0,0 +1,76 @@
+package com.dicoding.asclepius.view.detailNews
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.net.toUri
+import com.bumptech.glide.Glide
+import com.dicoding.asclepius.R
+import com.dicoding.asclepius.databinding.ActivityDetailNewsBinding
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+class DetailNewsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailNewsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val title = intent.getStringExtra(TITLE)
+        val author = intent.getStringExtra(AUTHOR)
+        val imageString = intent.getStringExtra(IMAGE)
+        val sourceName = intent.getStringExtra(SOURCE_NAME)
+        val publishedAt = intent.getStringExtra(PUBLISHED_AT)
+        val description = intent.getStringExtra(DESCRIPTION)
+
+        val cleanDescription = description?.let { cleanText(it) }
+        val date = publishedAt?.let { getDate(it) }
+
+        val sourceFormat = getString(R.string.source)
+
+        if (imageString != "null") {
+            val imageUri = imageString?.toUri()
+            Glide.with(this)
+                .load(imageUri)
+                .into(binding.ivNews)
+        }
+
+        with(binding) {
+            tvTitle.text = title
+            tvAuthor.text = author
+            tvPublishedAt.text = date
+            tvSource.text = String.format(sourceFormat, sourceName)
+            tvDesc.text = cleanDescription
+        }
+    }
+
+    private fun cleanText(text: String): String {
+        return text
+            .replace("\n", "")
+            .replace("\t", "")
+            .replace("\r", "")
+            .replace("\\n", "")
+            .replace("\\r", "")
+            .replace("\\t", "")
+    }
+
+    private fun getDate(dateAndTime: String): String {
+        val instant = Instant.parse(dateAndTime)
+        val localDateTime = LocalDateTime.ofInstant(instant, ZoneId.of("UTC"))
+        val outputDateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+        return localDateTime.format(outputDateFormat)
+    }
+
+    companion object {
+        const val TITLE = "title"
+        const val AUTHOR = "author"
+        const val IMAGE = "image"
+        const val SOURCE_NAME = "source_name"
+        const val PUBLISHED_AT = "published_at"
+        const val DESCRIPTION = "description"
+    }
+}
